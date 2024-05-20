a4838997c2001a8641fa84bf4608f11e203594f2
Mykhailo33 <mykhailo.zadorozhnii.tr.2022@lpnu.ua>
2024-05-20 19:18:12 +0300

    Initial commit

---
+++ src/Task2.java
@@ -0,0 +1,24 @@
+public class Task2 {
+    public static void main(String[] args) {
+        // Придумуємо речення з 10-12 слів і зберігаємо кожне слово в змінній
+        String word1 = "Сонце";
+        String word2 = "світить";
+        String word3 = "яскраво";
+        String word4 = "на";
+        String word5 = "небі";
+        String word6 = "і";
+        String word7 = "просинає";
+        String word8 = "всіх,";
+        String word9 = "дерева";
+        String word10 = "і";
+        String word11 = "квіти.";
+
+        // Формуємо речення, об'єднуючи слова
+        String sentence = word1 + " " + word2 + " " + word3 + " " + word4 + " " + word5 + " "
+                + word6 + " " + word7 + " " + word8 + " " + word9 + " " + word10 + " " + word11;
+
+        // Виводимо речення в консоль
+        System.out.println(sentence);
+    }
+}
+
